bc44746a6bfd0ab8264fdcc92e0ae5d281eee325
Romain Bada <151039225+Aarkhez@users.noreply.github.com>
2026-03-15 15:39:42 +0100

    Rename 'Team Norxus' to 'Team Marathon'

--- src/data/teams.ts
+++ src/data/teams.ts
@@ -2,7 +2,7 @@ import { Team } from '../types/team';
 
 export const TEAMS: Team[] = [
   { id: 1, name: 'Team Mastos', slug: '' },
-  { id: 2, name: 'Team Norxus', slug: '' },
+  { id: 2, name: 'Team Marathon', slug: '' },
 ];
 
 export default TEAMS;
